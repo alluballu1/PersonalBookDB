d07a04b7c77ab41e4295285da2f5414581e449e8
alluballu1 <45081751+alluballu1@users.noreply.github.com>
2022-02-07 12:19:01 +0200

    base page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Container } from "react-bootstrap";
+import BasicSpeedDial from "./components/SpeedDial";
+import ReactSelect from "react-select";
+import image from "./media/images/wallpaperflare.com_wallpaper.jpg";
+import { Box } from "@mui/material";
 
 function App() {
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
   return (
-    <div className="App">
-      <div>
-        hallo
-      </div>
-    </div>
+    <>
+      <img
+        src={image}
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          zIndex: -10000,
+        }}
+      />
+      <Container
+        style={{
+          background: "rgb(255,255,255, 0.85)",
+          justifyContent: "center",
+          margin: "auto",
+          width: "75vw",
+          height: "100vh",
+        }}
+        
+      >
+        <Box>
+        <div>
+          <h1>
+            TEST
+          </h1>
+        </div>
+        <div style={{ width: "50%", margin: "auto", marginTop:"10%" }}>
+          <ReactSelect
+            closeMenuOnSelect={false}
+            styles={{ background: "pink" }}
+            options={options}
+            isMulti={true}
+          />
+        </div>
+        </Box>
+
+
+        <BasicSpeedDial />
+      </Container>
+    </>
   );
 }
 
