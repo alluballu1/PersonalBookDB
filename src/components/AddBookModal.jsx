817dbd0c381b24c619644d1beb26a2c2debd3fff
alluballu <axu.porri@gmail.com>
2022-02-15 15:52:29 +0200

    Update AddBookModal.jsx

--- src/components/AddBookModal.jsx
+++ src/components/AddBookModal.jsx
@@ -3,6 +3,11 @@ import { Button, Form, Modal } from "react-bootstrap";
 
 const AddBookModal = () => {
   const [show, setshow] = useState(true);
+  const [genres, setGenres] = useState([]);
+
+  const addToArr = (val, type) => {
+
+  } 
 
   const closeModal = () => setshow(!show);
   return (
@@ -12,26 +17,25 @@ const AddBookModal = () => {
       </Modal.Header>
       <Modal.Body>
         <Form onSubmit={null}>
-          <Form.Text>Username</Form.Text>
-          <Form.Control
-            
-          />
-          <Form.Text>Password</Form.Text>
-          <Form.Control
-           
-          />
+          <Form.Text>Name</Form.Text>
+          <Form.Control />
+          <Form.Text>Author</Form.Text>
+          <Form.Control />
+          <Form.Text>Genre(s)</Form.Text>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <Form.Control />
+            <Button variant="dark">Add</Button>
+          </div>
+          {genres.map((element) => (
+            <div>{element}</div>
+          ))}
           <div
             style={{ display: "flex", justifyContent: "flex-end", padding: 10 }}
           >
-            <Button style={{ marginRight: 5, bg:"black" }} type="submit" variant="dark">
+            <Button style={{ marginRight: 5 }} type="submit" variant="dark">
               Add
             </Button>
-            <Button
-              
-              variant="secondary"
-            >
-              Cancel
-            </Button>
+            <Button variant="secondary">Cancel</Button>
           </div>
         </Form>
       </Modal.Body>
